664b23c85e42fc663417417e921c52aad443f73c
leandroarqia <looqzleandro16@gmail.com>
2026-06-05 15:58:00 +0000

    switch to tailwind v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
+  plugins: [react()],
 });
